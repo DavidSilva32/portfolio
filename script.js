ff1ab49d8e79577ff975b5c9f8127f09e807e5fb
DavidSilva32 <david.silvaxd32@gmail.com>
2023-03-08 07:08:37 -0300

    update

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function mudarTamanho() {
     if (window.innerWidth >= 768) {
-        itens.style.display = 'block'
+        itens.style.display = 'flex'
     } else {
         itens.style.display = 'none'
     }
 }
 
 function clickMenu() {
-    if (itens.style.display == 'block') {
+    if (itens.style.display == 'flex') {
+        burguer.style.width = "50px"
+        burguer.style.borderRadius = "50%"
+        burguer.style.backgroundColor = 'rgba(255, 251, 251, 0.055)'
+        burguer.style.color = 'white'
         itens.style.display = 'none'
     } else {
-        itens.style.display = 'block'
+        burguer.style.width = "250px"
+        burguer.style.borderRadius = "10px"
+        burguer.style.backgroundColor = "aquamarine"
+        burguer.style.color = 'black'
+        itens.style.display = 'flex'
     }
 }
